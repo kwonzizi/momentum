730abb25648387297d703d257920a8bf54192450
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2022-12-05 05:03:30 +0900

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ const images = ["봄.jpg","여름.jpg","가을.jpg","겨울.jpg"];
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
 const bgImg = document.createElement("img");
-bgImg.src = `momentum/${chosenImage}`;
+bgImg.src = `kwonzizi/momentum/${chosenImage}`;
 
 document.body.appendChild(bgImg);
 
