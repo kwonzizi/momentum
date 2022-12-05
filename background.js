4e10d89b7c968cb1887044dcdb2a6cc4b5c6c281
kwonzizi <117518951+kwonzizi@users.noreply.github.com>
2022-12-05 18:27:13 +0900

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ const images = ["spring.JPG","summer.JPG","fall.JPG","winter.JPG"];
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
 const bgImg = document.createElement("img");
-bgImg.src = `../${chosenImage}`;
+bgImg.src = `./img/${chosenImage}`;
 
 document.body.appendChild(bgImg);
 
